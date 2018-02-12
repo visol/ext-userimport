a0e8d6c8cb93b472d560efca912ff967f3ac90f2
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2018-02-12 13:37:05 +0000

    [TASK] Remove dead code

--- ext_tables.php
+++ ext_tables.php
@@ -2,8 +2,7 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
-    function()
-    {
+    function () {
 
         if (TYPO3_MODE === 'BE') {
 
@@ -14,22 +13,17 @@ call_user_func(
                 '', // Position
                 [
                     'Userimport' => 'main,upload,options,fieldMapping,importPreview,performImport',
-                    
+
                 ],
                 [
                     'access' => 'user,group',
                     'navigationComponentId' => null,
-                    'icon'   => 'EXT:userimport/Resources/Public/Icons/user_mod_userimport.svg',
+                    'icon' => 'EXT:userimport/Resources/Public/Icons/user_mod_userimport.svg',
                     'labels' => 'LLL:EXT:userimport/Resources/Private/Language/locallang_userimport.xlf',
                 ]
             );
-
         }
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('userimport', 'Configuration/TypoScript', 'Frontend User Import');
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_userimport_domain_model_userimport', 'EXT:userimport/Resources/Private/Language/locallang_csh_tx_userimport_domain_model_userimport.xlf');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_userimport_domain_model_userimport');
-
     }
 );
