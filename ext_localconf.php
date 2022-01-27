713e540666efbbcb3e0a4673d6cf616941eb591d
Andy Meier <andy.meier@visol.ch>
2022-01-27 15:23:43 +0100

    [BUGFIX] add missing typoscript imports

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,20 @@
 <?php
+
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use Visol\Userimport\Mvc\Property\TypeConverter\UploadedFileReferenceConverter;
+
 defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-    function()
-    {
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter(\Visol\Userimport\Mvc\Property\TypeConverter\UploadedFileReferenceConverter::class);
-    }
-);
+(function ($extKey = 'userimport') {
+    ExtensionUtility::registerTypeConverter(
+        UploadedFileReferenceConverter::class
+    );
+
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        '@import \'EXT:userimport/Resources/Private/TypoScript/setup.ts\''
+    );
+
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants(
+        '@import \'EXT:userimport/Resources/Private/TypoScript/constants.ts\''
+    );
+})();
